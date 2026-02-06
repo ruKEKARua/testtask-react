cf674ac1643140639de051159049f2fb32294de7
Ivan Kekar <vdeynesheko20@gmail.com>
2026-02-06 12:06:28 +0400

    project created and make header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import './UI/App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+          
+            <header>
+
+                <div className='logo'>
+                </div>
+
+                <div className='header_buttons'>
+
+                    <div className='contacts_button second_text'>
+
+                        <a href="">Контакты</a>
+
+                    </div>
+
+                    <div className='join_button main_text'>
+
+                        <a href="">Войти</a>
+
+                    </div>
+
+                </div>
+
+            </header>
+
+        </div>
+    );
 }
 
 export default App;
