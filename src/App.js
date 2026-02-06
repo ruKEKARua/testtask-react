b6549abc22e8e39625b1bd2d6fc5f3959fec85fc
Ivan Kekar <vdeynesheko20@gmail.com>
2026-02-06 12:16:31 +0400

    decomprosed Header

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
+import Header from './Components/Header/Header';
 import './UI/App.css';
 
 function App() {
     return (
         <div className="App">
           
-            <header>
+            <Header />
 
-                <div className='logo'>
-                </div>
+            <main>
 
-                <div className='header_buttons'>
-
-                    <div className='contacts_button second_text'>
-
-                        <a href="">Контакты</a>
-
-                    </div>
-
-                    <div className='join_button main_text'>
-
-                        <a href="">Войти</a>
-
-                    </div>
+                <div>
+                    
+                    <h1 className='main_text'>Место для получения медицинской помощи</h1>
 
                 </div>
+                <div></div>
 
-            </header>
+            </main>
+
+
 
         </div>
     );
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+
+        <div className='logo'>
+        </div>
+    
+        <div className='header_buttons'>
+    
+            <div className='contacts_button second_text'>
+    
+                <a href="">Контакты</a>
+    
+            </div>
+    
+            <div className='join_button main_text'>
+    
+                <a href="">Войти</a>
+    
+            </div>
+    
+        </div>
+    
+    </header>
+  )
+}
+
+export default Header
